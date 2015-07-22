ce50e7ea6ca53047fccd901540ad49d1a2f7a057
Sheng Lin <812876100@qq.com>
2015-07-22 11:59:37 +0800

    circleImageDrawable

---
+++ app/src/main/java/com/ringares/coins/utils/CircleImageDrawable.java
@@ -0,0 +1,63 @@
+package com.ringares.coins.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by ls
+ * on 2015/7/21
+ * Description
+ */
+public class CircleImageDrawable extends Drawable {
+
+    private Paint mPaint;
+    private Bitmap mBitmap;
+    private int mWidth;
+    private RectF rectF;
+
+    public CircleImageDrawable(Bitmap mBitmap) {
+        this.mBitmap = mBitmap;
+        BitmapShader bitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setShader(bitmapShader);
+        mWidth = Math.min(mBitmap.getWidth(), mBitmap.getHeight());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(mWidth/2, mWidth/2, mWidth/2, mPaint);
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return mWidth;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return mWidth;
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        mPaint.setAlpha(alpha);
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+        mPaint.setColorFilter(cf);
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
